60ff815b083549336b71c3d788a6763386a5f5a2
KarthikRam14 <karthikram1441@gmail.com>
2024-02-22 23:13:49 +0530

    updated

--- kalvium-pronote/src/App.jsx
+++ kalvium-pronote/src/App.jsx
@@ -1,37 +1,65 @@
-import React, { Component } from 'react'
+import React from "react";
+import { Component } from "react";
+import "./App.css";
 
 export class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      value: ""
+      items: [],
     };
   }
 
-  enterItem = (event)=>{
+  enterItem = (event) => {
     this.setState({
-      value: event.target.value
-    })
-  }
+      value: event.target.value,
+    });
+  };
 
-  addItem(){
-    this.enterItem
-  }
+  addItem = () => {
+    if (this.state.value !== "") {
+      this.setState({
+        items: [...this.state.items, this.state.value],
+        value: "",
+      });
+    }
+  };
+
+  deleteItem = (index) => {
+    const items = [...this.state.items];
+    items.splice(index, 1);
+    this.setState({ items });
+  };
 
   render() {
     return (
       <div>
         <div className="top">
-          <input type="text" className='input' placeholder='Type here' onChange={this.enterItem}/>
-          <button className='add' onClick={this.addItem}>Add Item</button>
+          <input
+            type="text"
+            className="input"
+            placeholder="Type here"
+            onChange={this.enterItem}
+            value={this.state.value}
+          />
+          <button className="add" onClick={this.addItem}>
+            Add Item
+          </button>
         </div>
-        <input type="text">{this.value}</input>
-        <button>Delete Item</button>
-        </div>
-    )
+        <ul>
+          {this.state.items.map((item, index) => (
+            <div className="items" key={index}>
+              {item}
+              <button className="delete" onClick={() => this.deleteItem(index)}>
+                Delete
+              </button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
